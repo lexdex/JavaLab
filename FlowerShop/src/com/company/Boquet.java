0d58cecfde6fc0fb7877b5008caba39fa563f827
Lex Dex <41371569+lexdex@users.noreply.github.com>
2019-02-19 11:34:20 +0200

    Add files via upload

---
+++ FlowerShop/src/com/company/Boquet.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.ArrayList;
+
+public class Boquet {
+
+	
+	private List <Flower>flowerlist;
+	
+	Boquet(){}
+	void makeBoquet(){}
+	
+	void calculatePrice(){}
+}
